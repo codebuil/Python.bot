adac173124fcfa456e0a2b00a5aa628b7f47b05b
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2023-11-21 23:32:36 +0000

    Add files via upload

--- browser.py
+++ browser.py
@@ -4,5 +4,13 @@ from selenium.webdriver.common.by import By
 
 print("\x1bc\x1b[43;30mbrowser")
 driver = webdriver.Firefox()
-driver.get("https://github.com/codebuil?tab=repositories")
+driver.get("https://defuse.ca/online-x86-assembler.htm")
+elem = driver.find_element(By.NAME, "instructions")
+elem.send_keys("mov eax,0")
+elem.send_keys(Keys.RETURN)
+elem = driver.find_element(By.NAME, "submit")
+elem.send_keys(Keys.RETURN)
+
+
+
 
