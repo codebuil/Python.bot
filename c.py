0eb19268a18cf6e199d820a06a0bdf0c2b94721b
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2023-12-01 12:51:29 +0000

    Add files via upload

--- c.py
+++ c.py
@@ -2,6 +2,7 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver import ActionChains
 codes="""
 int add(int a,int b){
 ret a+b;
@@ -12,17 +13,36 @@ driver = webdriver.Firefox()
 driver.get("https://godbolt.org/")
 t=True
 while(t):
-    elem = driver.find_element(By.XPATH,'//*[@id="alert"]')
+    elem = driver.find_element(By.XPATH,"/html/body/div[6]/div/div/div[1]/button")
     try:
         
-        elem.send_keys(Keys.ESCAPE)
+        elem.click()
 
         t=False
     except:
         t=True
 
+alert = driver.switch_to.default_content()
+elem = driver.find_element(By.XPATH,"/html/body/div[6]/div/div/div[1]/button")
+elem.click()
+t=True
+while(t):
+    elem = driver.find_element(By.XPATH,"/html/body/div[6]/div/div/div[1]/button")
+    try:
+        elem.click()
+        #elem.send_keys("\n")
+
+        t=False
+    except:
+        t=True
+t=True
+
+
 time.sleep(2)
+
+
+
 elem = driver.find_element(By.XPATH,"/html/body/div[1]/div/div/div[3]/div[2]/div/div/div[2]/div/div[1]/div[2]/div[1]")
 print(elem.text)
-driver.close()
+#driver.close()
 
