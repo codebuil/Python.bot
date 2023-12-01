5d4efdc8afacfbd10063e6ffbe6147ee0b37d471
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2023-12-01 10:09:47 +0000

    Add files via upload

--- cmd.py
+++ cmd.py
@@ -3,13 +3,24 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
+
+text="""
+mov eax,0
+mov ebx,0
+mov ecx,0
+mov edx,0
+mov esi,0
+mov edi,0
+
+"""
 print("\x1bc\x1b[43;30mWait....")
 driver = webdriver.Firefox()
 driver.get("https://defuse.ca/online-x86-assembler.htm")
 elem = driver.find_element(By.NAME, "instructions")
-elem.send_keys("mov eax,0")
+elem.send_keys(text)
 elem.send_keys(Keys.RETURN)
 elem = driver.find_element(By.NAME, "submit")
+time.sleep(2)
 elem.send_keys(Keys.RETURN)
 time.sleep(2)
 #/html/body/div[1]/div[3]/div[2]/div[2]/p[2]
